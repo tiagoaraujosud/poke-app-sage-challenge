0a7a12a0363d953fe44c8db1159b76740c8f7bd0
tiagoaraujosud <tiagoaraujosud@gmail.com>
2024-10-23 19:10:09 -0300

    feat: Create pokemonSlice

---
+++ poke-app-sage-challenge/src/state/pokemon/pokemonSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Ability {
+    ability: {
+      name: string;
+      url: string;
+    };
+}
+
+interface Type {
+    slot: number;
+    type: {
+        name: string;
+        url: string;
+    };
+}
+
+interface PokemonState {
+    name: string;
+    sprites: {
+      front_default: string;
+    };
+    height: number;
+    weight: number;
+    types: Type[],
+    abilities: Ability[];
+}
+
+const initialState: PokemonState = {
+    name: "",
+    sprites: {
+        front_default: "",
+      },
+      height: 0,
+      weight: 0,
+      types: [],
+      abilities: []
+}
+
+const pokemonSlice = createSlice({
+    name: "pokemon",
+    initialState,
+    reducers: {},
+});
+
+export default pokemonSlice.reducer;

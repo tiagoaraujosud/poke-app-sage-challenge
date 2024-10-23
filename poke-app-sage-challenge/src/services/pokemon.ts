333099aa5cb0828ce4fc897da65e9f45677e74dd
tiagoaraujosud <tiagoaraujosud@gmail.com>
2024-10-23 06:26:39 -0300

    feat: organize the interface pokemon

--- poke-app-sage-challenge/src/services/pokemon.ts
+++ poke-app-sage-challenge/src/services/pokemon.ts
@@ -1,9 +1,48 @@
 import axios from 'axios';
 
+interface Ability {
+    ability: {
+      name: string;
+      url: string;
+    };
+    is_hidden: boolean;
+    slot: number;
+  }
+
+  interface Form {
+    name: string;
+    url: string;
+  }
+
+  interface Stat {
+    base_stat: number;
+    effort: number;
+    stat: {
+      name: string;
+      url: string;
+    };
+  }
+
+  interface Type {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }
+
 export interface Pokemon {
-  name: string;
-  url: string;
-}
+    abilities: Ability[];
+    base_experience: number;
+    forms: Form[];
+    height: number;
+    id: number;
+    name: string;
+    order: number;
+    stats: Stat[];
+    types: Type[];
+    weight: number;
+  }
 
 export const getPokemonList = async (): Promise<Pokemon[]> => {
   try {

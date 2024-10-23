f33b6abe075ae91cf63b6d69e5a9f5b581bc1cb1
tiagoaraujosud <tiagoaraujosud@gmail.com>
2024-10-22 22:29:17 -0300

    feat: Pokemon route

---
+++ poke-app-sage-challenge/src/pages/Pokemon/Pokemon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pokemon = () => {
+    return(
+        <p>Pokemon</p>
+    )
+}
+
+export default Pokemon;
--- poke-app-sage-challenge/src/routes/app.routes.tsx
+++ poke-app-sage-challenge/src/routes/app.routes.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import List from '../pages/List/List';
-
+import Pokemon from '../pages/Pokemon/Pokemon';
 
 const AppRoutes = () => {
     return (
@@ -10,6 +10,7 @@ const AppRoutes = () => {
                 <Route >
                     <Route path="/" element={<Home />} />
                     <Route path="/list" element={<List />} />
+                    <Route path="/pokemon" element={<Pokemon />} />
                 </Route>
             </Routes>
         </BrowserRouter>
